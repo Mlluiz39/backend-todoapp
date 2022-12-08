4cb213607ffe870063ae84a3b6408f1154cf661e
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-12-08 14:40:30 -0300

    remove router private

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,7 +4,6 @@ const userController = require('../controllers/userController')
 
 router.get('/show/users', userController.get)
 router.get('/show/:id', userController.getById)
-router.get('/show/:id', userController.checkToken)
 router.post('/auth/register', userController.post)
 router.post('/auth/login', userController.login)
 router.put('/:id', userController.put)
