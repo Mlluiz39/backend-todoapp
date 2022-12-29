be9cb24b5aa51447a2124846d8f6b1c9f9e96b8c
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-12-29 16:46:57 -0300

    remove .env

--- src/db/database.js
+++ src/db/database.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const DB_USER = process.env.DB_USER
-const DB_PASS = process.env.DB_PASS
-const DB_NAME = process.env.DB_NAME
-
 module.exports = mongoose
   .connect(`mongodb://144.22.225.20:27017/mongo`, {
     useNewUrlParser: true,
