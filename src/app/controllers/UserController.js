0a614e2d52fa1da500749a2c409959bf7fbf6eb1
Marcelo Luiz <mlluizpereira39@gmail.com>
2023-01-08 17:03:27 -0300

    add yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,33 @@
 const { v4 } = require('uuid')
+const { object, string } = require('yup')
 
 const User = require('../models/User')
 
 class UserController {
   async store(req, res) {
+    const userSchema = object().shape({
+      name: string().required(),
+      email: string().email().required(),
+      password_hash: string().required().min(6),
+    })
+
+    // if (!(await userSchema.isValid(req.body))) {
+    //   return res.status(400).json({ error: 'Validation fails' })
+    // }
+
+    try {
+      await userSchema.validateSync(req.body, { abortEarly: false })
+    } catch (error) {
+      return res.status(400).json({ error: error.errors })
+    }
+
     const { name, email, password_hash } = req.body
+
+    const userExists = await User.findOne({ where: { email } })
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' })
+    }
 
     const user = await User.create({ id: v4(), name, email, password_hash })
 
